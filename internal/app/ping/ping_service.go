f6097f4f63c05c2e848d9f4c4dff57c4432a4b14
pkashirin <pkashirin@ozon.travel>
2020-12-05 16:36:57 +0300

    fix cd workflow

--- internal/app/ping/ping_service.go
+++ internal/app/ping/ping_service.go
@@ -4,21 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func Serve() {
-	http.HandleFunc("/", ping)
+	startDate := time.Now().Format("2006-01-02 15:04:05")
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		_, err := fmt.Fprint(writer, startDate)
+		if err != nil {
+			log.Printf("ping: %v", err)
+		}
+	})
 	go func() {
 		err := http.ListenAndServe(":80", nil)
 		if err != nil {
 			log.Printf("serve ping: %v", err)
 		}
 	}()
-}
-
-func ping(writer http.ResponseWriter, request *http.Request) {
-	_, err := fmt.Fprint(writer, "OK")
-	if err != nil {
-		log.Printf("ping: %v", err)
-	}
 }
